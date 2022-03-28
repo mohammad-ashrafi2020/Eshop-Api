8f77b005aafc871f2c11f8a5b858cf0dab855909
mohammad <mohammad.ashrafi2020@outlook.com>
2022-03-28 12:57:41 +0430

    Section 87 - ProductController

---
+++ Shop/Shop.Api/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Products.AddImage;
+using Shop.Application.Products.Create;
+using Shop.Application.Products.Edit;
+using Shop.Application.Products.RemoveImage;
+using Shop.Presentation.Facade.Products;
+using Shop.Query.Products.DTOs;
+
+namespace Shop.Api.Controllers;
+
+public class ProductController : ApiController
+{
+    private IProductFacade _productFacade;
+
+    public ProductController(IProductFacade productFacade)
+    {
+        _productFacade = productFacade;
+    }
+
+
+    [HttpGet]
+    public async Task<ApiResult<ProductFilterResult>> GetProductByFilter([FromQuery]ProductFilterParams filterParams)
+    {
+        return QueryResult(await _productFacade.GetProductsByFilter(filterParams));
+    }
+
+    [HttpGet("{productId}")]
+    public async Task<ApiResult<ProductDto?>> GetProductById(long productId)
+    {
+        var product = await _productFacade.GetProductById(productId);
+        return QueryResult(product);
+    }
+    [HttpGet("{slug}")]
+    public async Task<ApiResult<ProductDto?>> GetProductById(string slug)
+    {
+        var product = await _productFacade.GetProductBySlug(slug);
+        return QueryResult(product);
+    }
+
+    [HttpPost]
+    public async Task<ApiResult> CreateProduct([FromForm]CreateProductCommand command)
+    {
+        var result = await _productFacade.CreateProduct(command);
+        return CommandResult(result);
+    }
+
+    [HttpPost("images")]
+    public async Task<ApiResult> AddImage([FromForm] AddProductImageCommand command)
+    {
+        var result = await _productFacade.AddImage(command);
+        return CommandResult(result);
+    }
+
+    [HttpDelete("images")]
+    public async Task<ApiResult> RemoveImage(RemoveProductImageCommand command)
+    {
+        var result = await _productFacade.RemoveImage(command);
+        return CommandResult(result);
+    }
+    [HttpPut]
+    public async Task<ApiResult> EditProduct([FromForm] EditProductCommand command)
+    {
+        var result = await _productFacade.EditProduct(command);
+        return CommandResult(result);
+    }
+}
